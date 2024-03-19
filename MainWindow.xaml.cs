58b55a4bbe9cd9aa792f889118b49c288f296437
ArcaneAj <41121914+ArcaneAj@users.noreply.github.com>
2024-03-19 20:26:31 +1300

    Initial commit of mvp

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using FluentScheduler;
+using System.Windows;
+using System.Windows.Input;
+
+namespace RitalinReminder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var today = DateTime.Today.AddHours(14); // 2pm
+            InitializeComponent();
+            this.Hide();
+            JobManager.AddJob(
+                this.DoScheduledWork,
+                schedule => schedule.ToRunOnceAt(today).AndEvery(1).Days());
+            //schedule => schedule.ToRunNow().AndEvery(10).Seconds());
+        }
+
+        private void DoScheduledWork()
+        {
+            Dispatcher.Invoke(new Action(Show));
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            base.OnClosing(e);
+        }
+
+        private void CloseCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
